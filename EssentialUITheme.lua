3c94446dd105039d5768144303bf8a615e344ee6
Blissful <53982126+Blissful4992@users.noreply.github.com>
2021-05-16 16:42:30 +0200

    Update EssentialUITheme.lua

--- EssentialUITheme.lua
+++ EssentialUITheme.lua
@@ -1,1 +1,26 @@
+_G["Theme"] = {
+    ["Window_Border"] = Color3.fromRGB(166, 94, 0),
 
+    ["Section_Title"] = Color3.fromRGB(210, 210, 210),
+    ["Section_Background"] = Color3.fromRGB(0, 0, 0),
+
+    ["Item_Name_Color"] = Color3.fromRGB(255, 255, 255),
+
+    ["Button"] = Color3.fromRGB(166, 94, 0),
+
+    ["Toggle"] = Color3.fromRGB(166, 94, 0),
+
+    ["Color_Picker"] = Color3.fromRGB(166, 94, 0),
+    ["Color_Picker_Selector_Frame"] = Color3.fromRGB(166, 94, 0),
+
+    ["Slider_Bar"] = Color3.fromRGB(166, 94, 0),
+    ["Slider_Bob"] = Color3.fromRGB(255, 255, 255),
+    ["Slider_Value"] = Color3.fromRGB(160, 0, 255),
+
+    ["Keybind_Border"] = Color3.fromRGB(166, 94, 0),
+
+    ["Dropdown_Border"] = Color3.fromRGB(166, 94, 0),
+    ["Dropdown_Arrow"] = Color3.fromRGB(255, 255, 255),
+    ["Dropdown_Main_Option"] = Color3.fromRGB(255, 213, 153),
+    ["Dropdown_Options"] = Color3.fromRGB(255, 255, 255),
+}
